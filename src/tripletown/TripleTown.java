67001544e179fe6e1e93f238cba7e634bd36a183
erichsueh336 <erichsueh336@gmail.com>
2012-11-02 09:57:05 +0800

    first version

---
+++ src/tripletown/TripleTown.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tripletown;
+import tripletown.Map;
+
+/**
+ *
+ * @author apple
+ */
+public class TripleTown {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Map map = new Map();
+    }
+}
